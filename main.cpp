fa70b3cbb7e6b78e9ef539b423c6ce44607796f4
Paul Michel <paul.michel@tu-ilmenau.de>
2019-05-03 13:09:04 +0200

    file reading experiments

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 extern "C" {
     #include "spectral_common.h"
 }
+#include <iostream>
+#include <fstream>
 
 int main(int, char*[]) {
+    //std::ifstream scanfile("/sys/kernel/debug/ieee80211/phy1/ath10k");
+    std::ifstream scanfile("/sys/kernel/debug/ieee80211/phy1/ani_enable");
+
+    std::string line;
+    //std::getline(scanfile, line);
+    scanfile.get(line);
+    std::cout << line << std::endl;
 
 }
