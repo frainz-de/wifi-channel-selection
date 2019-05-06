e6324ffca5f233da43b4e89673403e2d39b8b8bc
Paul Michel <paul.michel@tu-ilmenau.de>
2019-05-06 12:51:11 +0200

    at least file opening works now

--- main.cpp
+++ main.cpp
@@ -3,14 +3,24 @@ extern "C" {
 }
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 int main(int, char*[]) {
     //std::ifstream scanfile("/sys/kernel/debug/ieee80211/phy1/ath10k");
-    std::ifstream scanfile("/sys/kernel/debug/ieee80211/phy1/ani_enable");
+    std::ifstream scanfile;
+    scanfile.open("/sys/kernel/debug/ieee80211/phy1/ath10k/spectral_scan0");
+    if(scanfile.fail()) {
+        std::cerr << "Failed to read file\n";
+        std::cerr << "Error: " << strerror(errno) << std::endl;
+        return 1;
+    }
 
     std::string line;
     //std::getline(scanfile, line);
-    scanfile.get(line);
+    //scanfile.get(line);
+    scanfile >> line;
     std::cout << line << std::endl;
+
+    scanfile.close();
 
 }
