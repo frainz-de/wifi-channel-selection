d716b5908a6c07a7c79a120cdf497e793311321b
Paul Michel <paul.michel@tu-ilmenau.de>
2019-05-07 19:30:46 +0200

    binary reading and tlv header handling

--- main.cpp
+++ main.cpp
@@ -8,17 +8,21 @@ extern "C" {
 int main(int, char*[]) {
     //std::ifstream scanfile("/sys/kernel/debug/ieee80211/phy1/ath10k");
     std::ifstream scanfile;
-    scanfile.open("/sys/kernel/debug/ieee80211/phy1/ath10k/spectral_scan0");
+    scanfile.open("/sys/kernel/debug/ieee80211/phy1/ath10k/spectral_scan0",std::fstream::in | std::fstream::binary);
     if(scanfile.fail()) {
         std::cerr << "Failed to read file: " << strerror(errno) << std::endl;
         return 1;
     }
 
-    std::string line;
-    //std::getline(scanfile, line);
-    //scanfile.get(line);
-    scanfile >> line;
-    std::cout << line << std::endl;
+
+    auto header = new fft_sample_tlv;
+    //auto header = static_cast<fft_sample_tlv*>(headerbuffer);
+    scanfile.read((char*)header, sizeof(fft_sample_tlv));
+    uint8_t type = header->type;
+    std::cout << "type: " << unsigned(type) << std::endl;
+    std::cout << "length: " << be16toh(header->length) << std::endl;
+
+    std::cout << "size of fft_sample_ath10k: " << sizeof(fft_sample_ath10k) << std::endl;
 
     scanfile.close();
 
