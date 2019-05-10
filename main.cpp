4cc68c05f5813222312262c0c978b2de3ed1ad1e
Paul Michel <paul.michel@tu-ilmenau.de>
2019-05-10 15:15:52 +0200

    put sample reading in its own function

--- main.cpp
+++ main.cpp
@@ -7,9 +7,22 @@ extern "C" {
 #include <stdio.h>
 #include <unistd.h>
 
-//int readSample(std::ifstream scanfile) {
-//
-//}
+fft_sample_ath10k* readSample(std::ifstream &scanfile) {
+
+    auto sample = new fft_sample_ath10k;
+    scanfile.read((char*)&sample->tlv, sizeof(fft_sample_tlv)); //read in header
+    std::cout << "type: " << unsigned(sample->tlv.type) << std::endl;
+    std::cout << "length: " << be16toh(sample->tlv.length) << std::endl;
+    if(sample->tlv.type != 3) {
+        throw std::runtime_error("Wrong sample type, only ath10k samples are supportet atm\n");
+    }
+
+    scanfile.read((char*)sample + sizeof(fft_sample_tlv), sizeof(*sample) - sizeof(fft_sample_tlv));
+    auto data = new char[be16toh(sample->tlv.length) - sizeof(fft_sample_ath10k) + sizeof(fft_sample_tlv)];
+//    scanfile.read(data, be16toh(sample->tlv.length) - sizeof(fft_sample_ath10k) + sizeof(fft_sample_tlv));
+    scanfile.read(data, 64);
+    return sample;
+}
 
 int main(int argc, char* argv[]) {
     std::string path = "/sys/kernel/debug/ieee80211/phy1/ath10k/spectral_scan0";
@@ -25,22 +38,8 @@ int main(int argc, char* argv[]) {
         std::cerr << "Failed to read file: " << strerror(errno) << std::endl;
         return 1;
     }
+    readSample(scanfile);
 
-
-    auto sample = new fft_sample_ath10k;
-    scanfile.read((char*)&sample->tlv, sizeof(fft_sample_tlv)); //read in header
-    std::cout << "type: " << unsigned(sample->tlv.type) << std::endl;
-    std::cout << "length: " << be16toh(sample->tlv.length) << std::endl;
-    if(sample->tlv.type != 3) {
-        //std::cerr << "Wrong sample type, only ath10k samples are supportet atm\n";
-        throw std::runtime_error("Wrong sample type, only ath10k samples are supportet atm\n");
-        return 2;
-    }
-
-    scanfile.read((char*)sample + sizeof(fft_sample_tlv), sizeof(*sample) - sizeof(fft_sample_tlv));
-    auto data = new char[be16toh(sample->tlv.length) - sizeof(fft_sample_ath10k) + sizeof(fft_sample_tlv)];
-//    scanfile.read(data, be16toh(sample->tlv.length) - sizeof(fft_sample_ath10k) + sizeof(fft_sample_tlv));
-    scanfile.read(data, 64);
 
     // scanfile.close(); // the destructor does this for us
     
