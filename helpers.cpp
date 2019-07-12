4bece96477b92669c47c127044b47ec10ff4a03a
Paul Michel <paul.michel@tu-ilmenau.de>
2019-07-12 12:46:41 +0200

    make helpers not header only anymore

---
+++ helpers.cpp
@@ -0,0 +1,27 @@
+#include "helpers.h"
+
+#include <array>
+#include <memory>
+#include <algorithm>
+
+
+void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+
+// execute system command and return stdout as string
+std::string exec(const std::string cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
--- helpers.h
+++ helpers.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <algorithm>
+#include <string>
 
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
-        return !std::isspace(ch);
-    }).base(), s.end());
-}
+void rtrim(std::string &s);
+
+
+// execute system command and return stdout as string
+std::string exec(const std::string cmd);
