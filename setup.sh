ef65a1393b87d91ae0bf0fb679658fa3fb9c4a26
Paul Michel <paul.michel@tu-ilmenau.de>
2019-07-16 16:27:51 +0200

    use other interface in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-cd /sys/kernel/debug/ieee80211/phy1/ath10k
-ip link set dev wlp5s0 up
+cd /sys/kernel/debug/ieee80211/phy0/ath10k
+ip link set dev wlp1s0 up
 echo background > spectral_scan_ctl
 echo trigger > spectral_scan_ctl
