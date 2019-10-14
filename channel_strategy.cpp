658553f81c2205374c5b0069340367c39c865a24
Paul Michel <paul.michel@tu-ilmenau.de>
2019-10-14 15:22:07 +0200

    use ht20 for net channel

--- channel_strategy.cpp
+++ channel_strategy.cpp
@@ -42,7 +42,7 @@ void ChannelStrategy::set_net_channel(int freq) {
         return;
     }
 
-    std::string res = exec("hostapd_cli -i " + netinterface + " chan_switch 3 " + std::to_string(freq));
+    std::string res = exec("hostapd_cli -i " + netinterface + " chan_switch 3 " + std::to_string(freq) + " ht20");
     rtrim(res);
     if (res == "OK") {
         std::cout << "\n\033[42msuccessfully set channel to " + std::to_string(freq) + "\033[0m\n";
